605b37ab979c087202a89fccfdbd995b868d39b5
NainaGhosh01 <nainaghosh01@gmail.com>
2025-04-18 17:41:30 +0530

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -6,8 +6,8 @@ usermod -a -G docker ec2-user
 systemctl enable docker
 
 # Login to ECR (replace with your region and AWS account ID in Terraform for dynamic)
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com
 
 # Pull and run your Strapi container
-docker pull ${aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/strapi:${image_tag}
-docker run -d -p 1337:1337 ${aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/strapi:${image_tag}
+docker pull ${aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/strapi:${image_tag}
+docker run -d -p 1337:1337 ${aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/strapi:${image_tag}
