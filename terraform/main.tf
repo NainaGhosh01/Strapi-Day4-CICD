5383725ae56d2ad8149d144d928df076271afb68
NainaGhosh01 <nainaghosh01@gmail.com>
2025-04-21 13:03:13 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,32 +2,20 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Fetch default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_instance" "strapi" {
-  ami                    = "ami-0df368112825f8d8f" # Amazon Linux 2023
-  instance_type          = var.instance_type
-  subnet_id              = data.aws_subnet_ids.default.ids[0]
-  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  key_name               = var.key_name
-
-  user_data = templatefile("${path.module}/user_data.sh", {
-    image_tag = var.image_tag
-    aws_account_id = "724772070195"
-  })
-
-  tags = {
-    Name = "Strapi-EC2"
+# Fetch subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
+# Create Security Group for Strapi
 resource "aws_security_group" "strapi_sg" {
   name   = "strapi-sg"
   vpc_id = data.aws_vpc.default.id
@@ -47,6 +35,7 @@ resource "aws_security_group" "strapi_sg" {
   }
 }
 
+# IAM Role for EC2 to access ECR and SSM
 resource "aws_iam_role" "ec2_role" {
   name = "strapi-ec2-role"
 
@@ -62,12 +51,39 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
+# Attach SSM access
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+# Attach ECR read access
+resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Instance Profile for EC2 to assume IAM Role
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "strapi-ec2-profile"
   role = aws_iam_role.ec2_role.name
+}
+
+# EC2 Instance for Strapi
+resource "aws_instance" "strapi" {
+  ami                    = "ami-01621ce8f257d0d13"  # Amazon Linux 2023
+  instance_type          = var.instance_type
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  key_name               = var.key_name
+
+  user_data = templatefile("${path.module}/user_data.sh", {
+    image_tag       = var.image_tag
+    aws_account_id  = "724772070195"
+  })
+
+  tags = {
+    Name = "Strapi-EC2"
+  }
 }
