e7569e836831bf3d4ef77473a6b799554a9a47ee
NainaGhosh01 <nainaghosh01@gmail.com>
2025-04-22 16:34:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,32 +47,20 @@ resource "aws_instance" "strapi" {
   key_name               = var.key_name
 
   user_data = <<-EOF
-  #!/bin/bash
-  yum update -y
-  yum install -y docker unzip curl
+    #!/bin/bash
+    yum update -y
+    yum install -y docker
+    systemctl start docker
+    systemctl enable docker
+    usermod -a -G docker ec2-user
 
-  systemctl start docker
-  systemctl enable docker
-  usermod -aG docker ec2-user
+    # Login to ECR
+    aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
 
-  # Install AWS CLI v2
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip
-  ./aws/install
-
-  # Configure AWS CLI using your access key (TEMPORARY - not ideal for prod)
-  aws configure set aws_access_key_id ${var.aws_access_key_id}
-  aws configure set aws_secret_access_key ${var.aws_secret_access_key}
-  aws configure set region ${var.aws_region}
-
-  # Login to ECR and pull the image
-  aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
-  docker pull ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/strapi:${var.image_tag}
-
-  # Run the Strapi container
-  docker run -d -p 1337:1337 ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/strapi:${var.image_tag}
-EOF
-
+    # Pull and run Strapi container
+    docker pull ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/strapi:${var.image_tag}
+    docker run -d -p 1337:1337 ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/strapi:${var.image_tag}
+  EOF
 
   tags = {
     Name = "Strapi-EC2"
